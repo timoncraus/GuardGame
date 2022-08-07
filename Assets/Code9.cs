e5f04568fcb670f5640de960bd28013f4a313eac
Mikhail Dunaev <74113682+STmihan@users.noreply.github.com>
2022-08-07 14:58:26 +0300

    Update Code9.cs

--- Assets/Code9.cs
+++ Assets/Code9.cs
@@ -6,22 +6,13 @@ public class Code9 : MonoBehaviour
 {
     public int x;
     public int y;
-    // Start is called before the first frame update
+
     void Start()
     {
         Debug.Log(isDivisor(x, y));
     }
     bool isDivisor(int x, int y)
     {
-        if(y%x == 0)
-        {
-            return true;
-        }
-        return false;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return y%x == 0;
     }
 }
